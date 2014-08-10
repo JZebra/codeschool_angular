daf845dee2d70c15dad8b92f3190a9d65ec86899
App Academy Student <student@appacademy.io>
2014-08-10 14:15:39 -0700

    finished level 2

--- testing.js
+++ testing.js
@@ -20,6 +20,18 @@
       canPurchase: false,
     }
   ];
+  
+  app.controller("PanelController", function(){
+    this.tab = 1;
+    
+    this.selectTab = function(setTab) {
+      this.tab = setTab;
+    };
+    
+    this.isSelected - function(checkTab) {
+      return this.tab === checkTab;
+    }
+  })
 
 })();
 
